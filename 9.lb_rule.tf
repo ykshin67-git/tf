38ade25008c624c6c07c66685942feeb5c12fdf8
ykshin67-git <82140281+ykshin67-git@users.noreply.github.com>
2021-08-26 17:41:20 +0900

    Update 9.lb_rule.tf

--- 9.lb_rule.tf
+++ 9.lb_rule.tf
@@ -1,11 +1,11 @@
 resource "azurerm_lb_rule" "lbrule" {
-    resource_group_name = azurerm_resource_group.david-rg.name
-    loadbalancer_id = azurerm_lb.david-lb.id
+    resource_group_name = azurerm_resource_group.user17-rg.name
+    loadbalancer_id = azurerm_lb.user17-lb.id
     name = "http"
     protocol = "Tcp"
     frontend_port = 80
     backend_port = 80
-    backend_address_pool_id = azurerm_lb_backend_address_pool.david-bpepool.id
-    frontend_ip_configuration_name = "davidPublicIPAddress"
-    probe_id = azurerm_lb_probe.david-lb-probe.id
+    backend_address_pool_id = azurerm_lb_backend_address_pool.user17-bpepool.id
+    frontend_ip_configuration_name = "user17PublicIPAddress"
+    probe_id = azurerm_lb_probe.user17-lb-probe.id
 }
