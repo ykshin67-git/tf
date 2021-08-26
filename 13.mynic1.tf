16f9ea43aa252127376dc8e33e0460bcdcf7bd02
ykshin67-git <82140281+ykshin67-git@users.noreply.github.com>
2021-08-26 17:43:24 +0900

    Update 13.mynic1.tf

--- 13.mynic1.tf
+++ 13.mynic1.tf
@@ -1,17 +1,17 @@
 resource "azurerm_network_interface" "nic1" {
     name                = "myNIC1"
-    location = azurerm_resource_group.david-rg.location
-    resource_group_name = azurerm_resource_group.david-rg.name
+    location = azurerm_resource_group.user17-rg.location
+    resource_group_name = azurerm_resource_group.user17-rg.name
 
     ip_configuration {
         name                          = "myNicConfiguration1"
-        subnet_id                     = azurerm_subnet.david-subnet1.id
+        subnet_id                     = azurerm_subnet.user17-subnet1.id
         private_ip_address_allocation = "Dynamic"
-#        public_ip_address_id          = azurerm_public_ip.david-publicip.id
+#        public_ip_address_id          = azurerm_public_ip.user17-publicip.id
     }
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "Created with Teraform"
     }
 }
 
@@ -23,7 +23,7 @@ resource "azurerm_network_interface_security_group_association" "nic-nsg-associa
 resource "azurerm_network_interface_backend_address_pool_association" "backendPool" {
   network_interface_id    = azurerm_network_interface.nic1.id
   ip_configuration_name   = "myNicConfiguration1"
-  backend_address_pool_id = azurerm_lb_backend_address_pool.david-bpepool.id 
+  backend_address_pool_id = azurerm_lb_backend_address_pool.user17-bpepool.id
 }
 resource "azurerm_network_interface_nat_rule_association" "natrule1" {
   network_interface_id  = azurerm_network_interface.nic1.id
