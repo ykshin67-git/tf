a697036e59858ebb53e0e310afb996a495897f0b
ykshin67-git <82140281+ykshin67-git@users.noreply.github.com>
2021-08-26 17:40:08 +0900

    Update 6.lb.tf

--- 6.lb.tf
+++ 6.lb.tf
@@ -1,9 +1,10 @@
-resource "azurerm_lb" "david-lb" {
-  name = "davidlb"
-  location = azurerm_resource_group.david-rg.location
-  resource_group_name = azurerm_resource_group.david-rg.name
+6.lb.tf
+resource "azurerm_lb" "user17-lb" {
+  name = "user17lb"
+  location = azurerm_resource_group.user17-rg.location
+  resource_group_name = azurerm_resource_group.user17-rg.name
   frontend_ip_configuration {
-  name = "davidPublicIPAddress"
-  public_ip_address_id = azurerm_public_ip.david-publicip.id
+  name = "user17PublicIPAddress"
+  public_ip_address_id = azurerm_public_ip.user17-publicip.id
  }
 }
