662a6a5f6078f257a3725874d42c8794e68eb66e
ykshin67-git <82140281+ykshin67-git@users.noreply.github.com>
2021-08-26 17:45:05 +0900

    Update 17.vm1.tf

--- 17.vm1.tf
+++ 17.vm1.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_machine" "web1" {
-    name                  = "davidweb1"
-    location              = azurerm_resource_group.david-rg.location
-    resource_group_name   = azurerm_resource_group.david-rg.name
+    name                  = "user17web1"
+    location              = azurerm_resource_group.user17-rg.location
+    resource_group_name   = azurerm_resource_group.user17-rg.name
     availability_set_id   = azurerm_availability_set.avset.id
     delete_os_disk_on_termination    = true
     network_interface_ids = [azurerm_network_interface.nic1.id]
@@ -21,19 +21,19 @@ resource "azurerm_virtual_machine" "web1" {
     }
 
  os_profile {
-        computer_name  = "davidweb1"
-        admin_username = "azureuser"    ## 아래 34번 라인의 계정이름과 동일해야 함
-        admin_password = "Pass****"     ## 12자리이상, 특수문자, 숫자, 대문자 조합으로 생성 필요
-	custom_data= file("web.sh")     ## Terraform 실행하는 서버에 존재해야 함, 실행은 만들어지는 VM에서
+        computer_name  = "user17web1"
+        admin_username = "user17"    ## 아래 34번 라인의 계정이름과 동일해야 함
+        admin_password = "lpg0416710!@"     ## 12자리이상, 특수문자, 숫자, 대문자 조합으로 생성 필요
+        custom_data= file("web.sh") ## Terraform 실행하는 서버/디렉토리에 존재해야 함, 실행은 만들어지는 VM에서
     }
 
  os_profile_linux_config {
         disable_password_authentication = false
         ssh_keys {
-	    ## ssh-keygen -t rsa -b 4096 -m PEM   명령어를 통해 Private Key(id_rsa)와 Public Key(id_rsa.pub)파일 생성
+            ## ssh-keygen 명령어를 통해 Private Key(id_rsa)와 Public Key(id_rsa.pub)파일 생성
             ## 서버 접근을 위해 관리 서버에서 생성한 id_rsa.pub 파일을 가상서버로 복사
-            path     = "/home/azureuser/.ssh/authorized_keys"   ## 가상 서버에 복사되는 위치
-	    ## id_rsa.pub 파일 내용을 아래 key_data에 넣어줌 (무조건 한줄로!!!)
+            path     = "/home/user17/.ssh/authorized_keys"   ## 가상 서버에 복사되는 위치
+            ## id_rsa.pub 파일 내용을 아래 key_data에 넣어줌 (무조건 한줄로!!!)
             key_data = file("~/.ssh/id_rsa.pub")
         }
     }
@@ -43,7 +43,6 @@ resource "azurerm_virtual_machine" "web1" {
     }
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "Created with Teraform"
     }
 }
-
