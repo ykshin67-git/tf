0f636273111981940916dfa3aea3aab4a03af757
ykshin67-git <82140281+ykshin67-git@users.noreply.github.com>
2021-08-26 17:41:53 +0900

    Update 10.mynsg.tf

--- 10.mynsg.tf
+++ 10.mynsg.tf
@@ -1,8 +1,8 @@
 resource "azurerm_network_security_group" "mynsg" {
     name                = "myNetworkSecurityGroup"
-    location            = azurerm_resource_group.david-rg.location
-    resource_group_name = azurerm_resource_group.david-rg.name
-    
+    location            = azurerm_resource_group.user17-rg.location
+    resource_group_name = azurerm_resource_group.user17-rg.name
+
     security_rule {
         name                       = "SSH"
         priority                   = 1001
@@ -11,7 +11,7 @@ resource "azurerm_network_security_group" "mynsg" {
         protocol                   = "Tcp"
         source_port_range          = "*"
         destination_port_range     = "22"
-        source_address_prefix      = "*"   ## Admin 서버 IP 주소 혹은 접속하는 서버(or PC) IP주소
+        source_address_prefix      = "52.231.51.192"   ## Admin 서버 IP 주소 혹은 접속하는 서버(or PC) IP주소
         destination_address_prefix = "*"
     }
  security_rule {
@@ -27,7 +27,6 @@ resource "azurerm_network_security_group" "mynsg" {
     }
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "Created with Teraform"
     }
 }
-
