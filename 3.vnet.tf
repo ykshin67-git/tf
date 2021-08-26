9298898adc14ce656c8b7c94d4a84b8e7e7c2daf
ykshin67-git <82140281+ykshin67-git@users.noreply.github.com>
2021-08-26 17:38:37 +0900

    Update 3.vnet.tf

--- 3.vnet.tf
+++ 3.vnet.tf
@@ -1,6 +1,6 @@
-resource "azurerm_virtual_network" "david-net" {
-    name = "david-vnet"
-    address_space = ["40.0.0.0/16"]
-    location = azurerm_resource_group.david-rg.location
-    resource_group_name = azurerm_resource_group.david-rg.name
+resource "azurerm_virtual_network" "user17-net" {
+    name = "user17-vnet"
+    address_space = ["17.0.0.0/16"]
+    location = azurerm_resource_group.user17-rg.location
+    resource_group_name = azurerm_resource_group.user17-rg.name
 }
