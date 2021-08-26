072cf52ff74d3e450a56f5d4defb0362cde6c912
ykshin67-git <82140281+ykshin67-git@users.noreply.github.com>
2021-08-26 17:44:35 +0900

    Update 16.myDiagStorage.tf

--- 16.myDiagStorage.tf
+++ 16.myDiagStorage.tf
@@ -1,12 +1,11 @@
 resource "azurerm_storage_account" "mystorageaccount" {
-    name                = "davidvm1diagestorage1"
-    resource_group_name = azurerm_resource_group.david-rg.name
-    location = azurerm_resource_group.david-rg.location
+    name                = "user17vm1diagestorage1"
+    resource_group_name = azurerm_resource_group.user17-rg.name
+    location = azurerm_resource_group.user17-rg.location
     account_replication_type = "LRS"
     account_tier = "Standard"
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "Created with Teraform"
     }
 }
-
